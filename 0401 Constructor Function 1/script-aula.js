d6b095d68fa096da69420fc6b8cd65eae5762c8d
schefferamado <amado.scheffer@gmail.com>
2025-06-21 13:59:26 -0300

    item adicionado

--- 0401 Constructor Function 1/script-aula.js
+++ 0401 Constructor Function 1/script-aula.js
@@ -1,10 +1,23 @@
 // Funcções cnstrutoras são utilizadas para a criação de objetos
+//obs: uma funcao vazia, por padrao, retorna undefined.
 
 //construtor 'Carro'
-function Carro(marca, preco) {
-  this.marca = marca;
-  this.preco = preco;
+// function Carro(marca, preco) {
+//   this.marca = marca;
+//   this.preco = preco;
+// }
+
+// const honda = new Carro("Honda", 3000);
+// const fiat = new Carro("Fiat", 2000);
+
+//construtor animal
+function Animal(raca, idade, preco) {
+  this.raca = raca;
+  this.idade = idade;
+  this.preco = 2000;
+
+  return console.log(raca, idade, preco);
 }
 
-const honda = new Carro("Honda", 3000);
-const fiat = new Carro("Fiat", 2000);
+const cachorro = new Animal("Pastor", 18);
+cachorro.porte = "grande";
--- 0401 Constructor Function 2/script-aula.js
+++ 0401 Constructor Function 2/script-aula.js
@@ -1,20 +1,56 @@
-//======= Criação d um objeto puro, sem construtor ========.
+// function Carro(marca, precoInicial) {
+//   this.marca = marca;
+//   const taxa = 1.2;
+//   const precoFinal = precoInicial * taxa;
+//   this.preco = precoFinal;
+//   console.log(this);
+// }
+
+// const fiat = new Carro("fiat", 200);
+
+//== Outro exemplo====
+// function Veiculo(tipo, cilindrada, ano, precoInicial) {
+//   const taxa = 1.4;
+//   this.tipo = tipo;
+//   this.cilindrada = cilindrada;
+//   this.ano = ano;
+//   const precoFinal = precoInicial * taxa;
+//   //preciso atribuir o this ao preco para que o valor consiga ser visível externamente.
+//   this.preco = precoFinal;
+//   return tipo, cilindrada, ano, precoFinal;
+// }
+
+// const moto = new Veiculo("suzuki", 1200, 1986, 1200);
+
+//====== Exemplo real para a utilização de objetos ========
+
+// criação de objeto:
+//obs: sempre que utilizarmos a palavra chave reservada this, estamos criando ou acesso variaveis do objeto.
 // const Dom = {
 //   seletor: "li",
+
 //   elemento() {
 //     return document.querySelector(this.seletor);
 //   },
-//   ativo() {
-//     this.elemento().classList.add("ativo");
+//   ativar() {
+//     this.elemento().classList.add("teste");
 //   },
 // };
-
-// Dom.ativo(); //adiciona ativo ao li
 // Dom.seletor = "ul";
-// Dom.ativo(); //adiciona ativo ao ul
+// Dom.ativar();
 
-// === Trasnformar o objeto acima em uma função construtora ===
+//==== Criando uma função construtora ========
 
 function Dom(seletor) {
-  this.ativa = function (seletor) {};
+  this.elemento = function () {
+    return document.querySelector(seletor);
+  };
+  //lembre-se sempre de utizar parametros(este é o poder do objeto)
+  this.ativar = function (classe) {
+    this.elemento().classList.add(classe);
+  };
 }
+
+const li = new Dom("li");
+const ul = new Dom("ul");
+ul.ativar("active");
