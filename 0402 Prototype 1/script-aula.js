024dc7c434038719b2f0764747902d6e9171c8ed
schefferamadoe <amado.scheffer@gmail.com>
2025-08-08 22:09:55 -0300

    atualizando menu

--- 0402 Prototype 1/script-aula.js
+++ 0402 Prototype 1/script-aula.js
@@ -1,10 +1,39 @@
 // o prototype é o núcleo do JavaScript
-// os protótipos estão ligados as funcções
+// os protótipos estão sempre ligados as funcções
+// Pode-se entender o prototype como um objeto que contém propriedades e métodos
+// que podem ser compartilhados entre todas as instâncias de uma função construtora
+// O prototype é um objeto que é adicionado automaticamente a todas as funções construtoras
+// O prototype é acessado através da propriedade prototype da função construtora
+// O prototype é usado para adicionar propriedades e métodos a todas as instâncias de uma função construtora
 
-function Pessoa(nome, idde) {
+function Pessoa(nome, idade) {
   this.nome = nome;
   this.idade = idade;
-}
-const adriano = new Pessoa("Adriano", 39);
 
+  this.hobby = function (tipo) {
+    return tipo;
+  };
+}
+
+console.log(Pessoa.prototype);
+
+// funcao.prototype:
+// É possível adicionar novas propriedades e métodos ao  objeto prototype.
+// EX:
+Pessoa.prototype.andar = function () {
+  return `${this.nome} está andando`;
+};
+
+Pessoa.prototype.nadar = function () {
+  return `${this.nome} nadou`;
+};
+Pessoa.prototype.treinou = function (esporte) {
+  return `${this.nome} treinou ${esporte}`;
+};
+
+Pessoa.prototype.profissao = function (profissaoTitle) {
+  return `A pessoa ${this.nome} tem a seguinte profissao ${profissaoTitle}`;
+};
+
+const adriano = new Pessoa("Adriano", 39);
 console.log(Pessoa.prototype);
